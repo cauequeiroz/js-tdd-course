19fdb57147355279cc9bbe50a751c48d4eec0d59
Caue Queiroz <cauenqueiroz@gmail.com>
2017-11-02 19:06:08 -0200

    Add test coverage with Nyc

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 const FizzBuzz = (number) => {
+  if (number === 0) return 0;
   if (number % 3 === 0 && number % 5 === 0) return 'FizzBuzz';
   if (number % 3 === 0) return 'Fizz';
   if (number % 5 === 0) return 'Buzz';
 
   return number;
+};
+
+const foo = (another) => {
+  return another.concat([]);
 };
 
 export default FizzBuzz;
