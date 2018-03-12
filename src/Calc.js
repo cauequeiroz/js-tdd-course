059b12cf1b6f639c558dc408d5340f890a80c369
Caue Queiroz <cauenqueiroz@gmail.com>
2018-03-11 23:32:59 -0300

    Fixing style

--- src/Calc.js
+++ src/Calc.js
@@ -1,8 +1,6 @@
 const sum = (num1, num2) => num1 + num2;
 const sub = (num1, num2) => num1 - num2;
 const mult = (num1, num2) => num1 * num2;
-const div = (num1, num2) => {
-  return (num2 === 0) ? 'You cannot divide by zero!' : num1 / 2
-};
+const div = (num1, num2) => ((num2 === 0) ? 'You cannot divide by zero!' : num1 / 2);
 
-export { sum, sub, mult, div }
+export { sum, sub, mult, div };
