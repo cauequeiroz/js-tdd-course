d707fb9ea0adf72e351cb70102b33abb3e36b14a
Caue Queiroz <cauenqueiroz@gmail.com>
2018-03-11 22:51:40 -0300

    Refactoring to ES6

--- src/Calc.js
+++ src/Calc.js
@@ -1,14 +1,8 @@
-module.exports = {
-  sum(num1, num2) {
-    return num1 + num2;
-  },
-  sub(num1, num2) {
-    return num1 - num2;
-  },
-  mult(num1, num2) {
-    return num1 * num2;
-  },
-  div(num1, num2) {
-    return (num2 === 0) ? 'You cannot divide by zero!' : num1 / 2;
-  },
+const sum = (num1, num2) => num1 + num2;
+const sub = (num1, num2) => num1 - num2;
+const mult = (num1, num2) => num1 * num2;
+const div = (num1, num2) => {
+  return (num2 === 0) ? 'You cannot divide by zero!' : num1 / 2
 };
+
+export { sum, sub, mult, div }
